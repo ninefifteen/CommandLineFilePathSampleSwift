2e453e2b2c126ddc2fca979f13746f1472e23a36
Shawn Seals <shawn.seals915@yahoo.com>
2015-02-01 11:10:39 -0600

    Initial Commit

---
+++ CommandLineFilePathSampleSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLineFilePathSampleSwift
+//
+//  Created by Shawn Seals on 2/1/15.
+//  Copyright (c) 2015 Shawn Seals. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
