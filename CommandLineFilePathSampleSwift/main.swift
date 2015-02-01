9c05687ed391259c760d3dcdfdbaa8913cd4d517
Shawn Seals <shawn.seals915@yahoo.com>
2015-02-01 14:05:00 -0600

    Initial commit.

--- CommandLineFilePathSampleSwift/main.swift
+++ CommandLineFilePathSampleSwift/main.swift
@@ -5,8 +5,57 @@
 //  Created by Shawn Seals on 2/1/15.
 //  Copyright (c) 2015 Shawn Seals. All rights reserved.
 //
+//  Run: ./CommandLineFilePathSampleSwift Arg1 Arg2 Arg3...
+//
 
 import Foundation
 
-println("Hello, World!")
+//*********************************************************************
+//  Read command line arguments method 1.
+//*********************************************************************
 
+let arguments = NSProcessInfo.processInfo().arguments
+for (index, argument) in enumerate(arguments) {
+    println("\(index): \(argument)")
+}
+
+//*********************************************************************
+
+
+
+//*********************************************************************
+//  Read command line arguments method 2.
+//*********************************************************************
+
+let argumentCount = C_ARGC
+println("argumentCount: \(argumentCount)")
+
+for var index = 0; index < Int(argumentCount); index++ {
+    let argument = String.fromCString(C_ARGV[index])
+    println("C_ARGV[\(index)]: \(argument)")
+}
+
+//*********************************************************************
+
+
+
+//*********************************************************************
+//  currentDirectoryPath returns home directory.
+//*********************************************************************
+
+let fileManager = NSFileManager()
+let currentPath = fileManager.currentDirectoryPath
+println("currentPath: \(currentPath)")
+
+//*********************************************************************
+
+
+
+//*********************************************************************
+//  bundlePath returns directory in which app is located.
+//*********************************************************************
+
+let appPath = NSBundle.mainBundle().bundlePath
+println("appPath: \(appPath)")
+
+//*********************************************************************
